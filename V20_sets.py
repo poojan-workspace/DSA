ee3dc9aa1ce95a86d3d5135d764d0d7823b539d4
Poojan Patel <poojan.workspace@gmail.com>
2026-05-11 16:49:42 -0700

    V20

--- V20_sets.py
+++ V20_sets.py
@@ -2,7 +2,7 @@
 Sets are mutable: that means we can change the values inside the set and the reference will still be the same of that set.
 set = {}
 
-Intersection and Union: common values or elements and all the values or elements without repitition respectively.
+Intersection (&) and Union (|): common values or elements and all the values or elements without repitition respectively.
 '''
 
 essential_spices = {"cardomom", "ginger", "cinnamom"}
@@ -13,3 +13,9 @@ print(f"Union of both sets: {all_spices}")
 
 common_spices = essential_spices & optional_spices # INTERSECTION
 print(f"Intersection of both sets: {common_spices}")
+
+
+only_in_essentials = essential_spices - optional_spices # A - B
+print(f"Only in essential spices set: {only_in_essentials}")
+
+print(f"Is 'cloves' in essential_spices set? {'cloves' in essential_spices}")
