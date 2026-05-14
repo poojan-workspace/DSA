d03a65b3d418b13961fb997121913a65233f18da
Poojan Patel <poojan.workspace@gmail.com>
2026-05-13 21:19:07 -0700

    V37

--- S5_Loops/V36_break_continue.py
+++ S5_Loops/V36_break_continue.py
@@ -4,19 +4,24 @@ break: completely breaks out of the loop.
 '''
 
 flavors = ["mango", "orange", "apple"]
+
 discontinued = ["banana", "strawberry"]
+
 out_of_stock = ["kiwi"]
+
 
 temp = True
 
+
 while temp == True:
+
     user = input("Enter the flavor you want: ").lower()
+
 
     if user in flavors:
         print(f"Great choice! {user} flavor ice cream is on the way.")
-    
     if user in discontinued:
         continue
-
     if user in out_of_stock:
         break
+print(f"Success!")
