6d5f8422f1cb513acd531ee0f12a2500795ce5a2
Poojan Patel <poojan.workspace@gmail.com>
2026-05-14 16:38:06 -0700

    V43

--- S6_Functions/V43_arguments.py
+++ S6_Functions/V43_arguments.py
@@ -17,3 +17,13 @@ def make_chai(tea, milk, sugar):
 
 make_chai("Ginger", "Yes", 2) # Positioning
 make_chai(sugar=3, tea="Lemon", milk="Yes please") # Keywords
+
+
+# *args and **kwargs: Arguments and Keyword Arguments
+
+def chai_order(*ingredients, **extras):
+    print("Ingredients are:", ingredients)
+    print("Extras are:", extras)
+
+chai_order("Cinnamon", "Ginger", sugar='2', masala='Yes') # Write in order, first all without keywords will go under "ingredients" variable
+                                                        #   Second all with the keywords will go under "extras" variable
