56ff25cfe7623ac25c3046f3cedacad0ed1b3e1a
Poojan Patel <poojan.workspace@gmail.com>
2026-07-01 14:38:48 -0700

    V63 changed

--- S9_OOPS/V63_self_args.py
+++ S9_OOPS/V63_self_args.py
@@ -4,18 +4,18 @@ A method always has "self" in its arguments.
 '''
 
 class Chaicup:
-    size = 150
+    size = 150 #property
 
     def describe(self):
         return f"The size of the cup is {self.size}"
     
-cup = Chaicup()
+cup = Chaicup() #object
 print(cup.describe())
-print(Chaicup.describe()) # this will give an error as it does not hacve any reference on who is calling it..
+'''print(Chaicup.describe())''' # this will give an error as it does not have any reference on who is calling it..
 
 # Fix:
 print(Chaicup.describe(cup))
 
 cup_2 = Chaicup()
-cup_2 = 100
+cup_2.size = 100
 print(Chaicup.describe(cup_2)) # that is why reference is very necessary as the same method or property can have different values
